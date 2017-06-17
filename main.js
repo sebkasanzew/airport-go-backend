b1f51296c47e2858c9969c005af760ccc83083ca
Sebastian Kasanzew <sebkasanzew@gmail.com>
2017-06-17 12:26:56 +0200

    Add meta data for heroku

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-require('node-fetch');
+let fetch = require('node-fetch')
 
-console.log("hello world");
+fetch('https://api.github.com/users/github')
+    .then(function(res) {
+      return res.json()
+    }).then(function(json) {
+      console.log(json)
+    }
+)
+
+let express = require('express');
+let app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/airport', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/puzzles', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
